17295b88b51bf5faa049d70321be2fd1149622ee
suuuuuuuuuuu <294490452@qq.com>
2021-04-05 17:10:21 +0800

    jz59-67

---
+++ src/main/java/jzoffer/J65_hasPath.java
@@ -0,0 +1,58 @@
+package jzoffer;
+
+/**
+ * @description:
+ * @author: suyue
+ * @time: 2021/4/4 14:06
+ */
+public class J65_hasPath {
+
+    public static boolean hasPath(char[][] matrix, String word) {
+        //方法：路径问题，dfs，递归。考虑到word在matrix中路径是否存在，可分解为word第一个字符matrix存在，且word剩下字符在第一个字符的四个方向路径存在，所以采用递归。
+        //递归函数的入参和返回值，功能：从起始位置matrix[row][ol]开始，是否存在word字串路径
+        //递归终止条件：loc == word.length()，说明路径存在；row和col不能超出数组边界；已经访问过的字符不能再次访问。
+        //递归单层逻辑：当前下标字符相等，则标记matrix该位置已访问，并且当前位置上下左右有一个方向的字符等于word中下一个字符。
+        if (matrix == null || matrix.length == 0 || word == null) return false;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(matrix, i, j, visited, word, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean dfs(char[][] matrix, int row, int col, boolean[][] visited, String word, int index) {
+        if (index == word.length()) return true;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        if (row < 0 || row >= rows || col < 0 || col >= cols) return false;
+        if (visited[row][col]) return false;
+        if (matrix[row][col] == word.charAt(index)) {
+            visited[row][col] = true;
+            if (dfs(matrix, row - 1, col, visited, word, index + 1)
+                    || dfs(matrix, row + 1, col, visited, word, index + 1)
+                    || dfs(matrix, row, col - 1, visited, word, index + 1)
+                    || dfs(matrix, row, col + 1, visited, word, index + 1)) {//四个方向有一个方向路径存在
+                return true;
+            }
+            visited[row][col] = false;
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        char[][] matrix = {{'a', 'b', 'c', 'e'}, {'s', 'f', 'c', 's'}, {'a', 'd', 'e', 'e'}};
+        String word = "abcced";
+        boolean result = hasPath(matrix, word);
+        if (result) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+}
---
+++ src/main/java/jzoffer/J67_cutRope.java
@@ -0,0 +1,75 @@
+package jzoffer;
+
+/**
+ * @description:
+ * @author: suyue
+ * @time: 2021/4/5 15:24
+ */
+public class J67_cutRope {
+
+    public static int cutRope(int target) {
+        //方法1：递归。时间复杂度太大了，不行的。
+        //递归函数功能：绳子长度为n，分别m段，最大乘积
+        //递归终止条件：n等于0，m等于0
+        //递归单层逻辑：假设最后一段长度为k，那么最大乘积为：k*helpCutRope(n-k,m-1)
+        /*int max = 0;
+        for (int m = 2; m <= target; m++) {
+            max = Math.max(max, helpCutRope(target, m));
+        }
+        return max;*/
+
+        //方法2：递归。时间复杂度较方法1小一点。m就是个烟雾弹呀！但是时间复杂度依然太大而超时了！
+        //递归函数功能：绳子长度为n的最大乘积
+        //递归终止条件：n<=4时，最大乘积为n。
+        //递归单层逻辑：假设最后一段长为k，那么最大乘积为：k*helpCutRope(n-k)，k的取值范围[1,n-1]
+        /*if (target == 2) return 1;
+        if (target == 3) return 2;
+        return helpCutRope(target);*/
+
+        //方法3：动态规划。发现递归过程有很多重复计算的过程，那就用数据存下来。
+        if (target == 2) return 1;
+        if (target == 3) return 2;
+        return helpCutRope(target);
+
+    }
+
+    private static int helpCutRope(int n) {
+        int[] dp = new int[n+1];
+        for (int i = 1; i <= 4; i++) {
+            dp[i] = i ;
+        }
+        for (int i = 5; i < n+1; i++) {
+            int tempMax = 0;
+            for (int j = 1; j < i; j++) {
+                tempMax = Math.max(tempMax, j * dp[i - j]);
+            }
+            dp[i] = tempMax;
+        }
+        return dp[n];
+    }
+
+    /*private int helpCutRope(int n) {
+        if (n <= 4) return n;
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, i * helpCutRope(n - i));
+        }
+        return max;
+    }*/
+
+    /*private int helpCutRope(int n, int m) {
+        if (m == 1) return n;
+        int tempMax = 0;
+        for (int i = 1; i <= n - m + 1; i++) {
+            tempMax = Math.max(tempMax, i * helpCutRope(n - i, m - 1));
+        }
+        return tempMax;
+    }*/
+
+    public static void main(String[] args) {
+        int n = 8;
+        int result = cutRope(n);
+        System.out.println(result);
+    }
+
+}
